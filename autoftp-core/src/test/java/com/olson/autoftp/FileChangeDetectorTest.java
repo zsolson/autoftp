696f39bf480e02cd6417b782cba2c362ca2580e8
Zack <Zack@Zack-PC.gateway.2wire.net>
2014-04-25 17:06:55 -0500

    Updated test to verify change times

--- autoftp-core/src/test/java/com/olson/autoftp/FileChangeDetectorTest.java
+++ autoftp-core/src/test/java/com/olson/autoftp/FileChangeDetectorTest.java
@@ -14,16 +14,22 @@ import org.junit.Test;
 public class FileChangeDetectorTest {
 
 	@Test
-	public void test() throws IOException
+	public void test() throws IOException, InterruptedException
 	{
 		Calendar calLastCheck = GregorianCalendar.getInstance();
+		
+		pause();
+		
 		File baseDir = new File("C:\\Users\\Zack\\Desktop\\FileChangeTest");
+		File file2 = null;
+		File file4 = null;
+		File file6 = null;
 		if (!baseDir.exists())
 		{
 			baseDir.mkdirs();
 			File file1 = new File("C:\\Users\\Zack\\Desktop\\FileChangeTest\\testFile1.txt");
 			file1.createNewFile();
-			File file2 = new File("C:\\Users\\Zack\\Desktop\\FileChangeTest\\testFile2.txt");
+			file2 = new File("C:\\Users\\Zack\\Desktop\\FileChangeTest\\testFile2.txt");
 			file2.createNewFile();
 			File innerDir = new File("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir");
 			if (!innerDir.exists())
@@ -31,7 +37,7 @@ public class FileChangeDetectorTest {
 				innerDir.mkdirs();
 				File file3 = new File("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir\\testFile3.txt");
 				file3.createNewFile();
-				File file4 = new File("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir\\testFile4.txt");
+				file4 = new File("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir\\testFile4.txt");
 				file4.createNewFile();
 				File innerInnerDir = new File("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir\\innerInnerDir");
 				if (!innerInnerDir.exists())
@@ -39,7 +45,7 @@ public class FileChangeDetectorTest {
 					innerInnerDir.mkdirs();
 					File file5 = new File("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir\\innerInnerDir\\testFile5.txt");
 					file5.createNewFile();
-					File file6 = new File("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir\\innerInnerDir\\testFile6.txt");
+					file6 = new File("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir\\innerInnerDir\\testFile6.txt");
 					file6.createNewFile();
 				}
 			}
@@ -54,8 +60,30 @@ public class FileChangeDetectorTest {
 		assertTrue(collChangedFiles.contains("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir\\testFile4.txt"));
 		assertTrue(collChangedFiles.contains("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir\\innerInnerDir\\testFile5.txt"));
 		assertTrue(collChangedFiles.contains("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir\\innerInnerDir\\testFile6.txt"));
+
+		calLastCheck = GregorianCalendar.getInstance();
+		pause();
+		
+		FileUtils.touch(file2);
+		FileUtils.touch(file4);
+		FileUtils.touch(file6);
+		File file7 = new File("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir\\innerInnerDir\\testFile7.txt");
+		file7.createNewFile();
+		
+		collChangedFiles = changeDetector.findChangesAfter(calLastCheck);
+		assertTrue(collChangedFiles.contains("C:\\Users\\Zack\\Desktop\\FileChangeTest\\testFile2.txt"));
+		assertTrue(collChangedFiles.contains("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir\\testFile4.txt"));
+		assertTrue(collChangedFiles.contains("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir\\innerInnerDir\\testFile6.txt"));
+		assertTrue(collChangedFiles.contains("C:\\Users\\Zack\\Desktop\\FileChangeTest\\innerDir\\innerInnerDir\\testFile7.txt"));
+		
+		
 		
 		FileUtils.deleteDirectory(baseDir);
+	}
+	
+	private void pause() throws InterruptedException
+	{
+		Thread.sleep(1000);
 	}
 
 }
